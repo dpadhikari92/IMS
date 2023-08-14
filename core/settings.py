9d3366255d79a3cf631dc7ab325d0cdda906cecc
Durga <dpadhikari92@gmail.com>
2023-08-14 19:22:10 +0530

    static files

--- core/settings.py
+++ core/settings.py
@@ -11,11 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from pathlib import Path
 
 from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -152,8 +151,7 @@ USE_L10N = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR ,'staticfiles_build')
-STATICFILES_DIRS=[os.path.join(BASE_DIR ,'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR ,"staticfiles_build", "static")
 
 
 # bootstrap template crispy-form uses
