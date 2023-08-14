5ab7eae2b9be7a853cbab1fc712d15e24b8ec8b9
Durga <dpadhikari92@gmail.com>
2023-08-14 18:50:09 +0530

    file name change

--- core/settings.py
+++ core/settings.py
@@ -151,7 +151,7 @@ USE_L10N = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR ,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR ,'staticfiles_build')
 STATICFILES_DIRS=[os.path.join(BASE_DIR ,'static')]
 
 
