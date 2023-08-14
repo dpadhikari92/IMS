ae3a879266123695fa501de10078169c950e7efe
Durga <dpadhikari92@gmail.com>
2023-08-14 18:47:53 +0530

    major change

--- core/settings.py
+++ core/settings.py
@@ -151,7 +151,8 @@ USE_L10N = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR ,"staticfiles_build", "static")
+STATIC_ROOT = os.path.join(BASE_DIR ,'staticfiles')
+STATICFILES_DIRS=[os.path.join(BASE_DIR ,'static')]
 
 
 # bootstrap template crispy-form uses
